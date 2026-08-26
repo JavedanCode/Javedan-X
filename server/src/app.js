4217695754aed9cf4722b142589e9bdb481d8cda
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-26 14:52:15 +0300

    Mount post routes

--- server/src/app.js
+++ server/src/app.js
@@ -10,6 +10,7 @@ import { configurePassport } from './config/passport.js';
 
 import authRoutes from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
+import postRouter from './routes/post.routes.js';
 
 import { errorHandler } from './middleware/error-handler.js';
 
@@ -40,6 +41,7 @@ app.use(passport.initialize());
 
 app.use('/auth', authRoutes);
 app.use('/users', userRouter);
+app.use('/posts', postRouter);
 
 app.get('/health', (req, res) => {
   res.status(200).json({
