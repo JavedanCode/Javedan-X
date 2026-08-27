b5376dadbdbc16781922cfc5c9c2e405f1185d68
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-27 17:46:56 +0300

    Add basename to Browser router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import AppRouter from "./router/AppRouter.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Javedan-X">
       <AuthProvider>
         <AppRouter />
       </AuthProvider>
